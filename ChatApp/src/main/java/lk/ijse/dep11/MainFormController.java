2c5ef96cc42d3c35851c9168983de241b50694cf
Dewuruge <dewurugekalpadeep@gmail.com>
2023-09-25 18:26:49 +0530

    Create Interface

---
+++ ChatApp/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep11;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane root;
+    public TextArea txtChat;
+    public TextField txtMessage;
+    public Button btnSend;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
